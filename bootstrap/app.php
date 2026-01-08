c4bf8496eecf3965630ef961e74587e07d441bc0
Nicholas Andrian Surjadi <surjadin@Mac.dana.local>
2026-01-08 14:33:42 +0700

    fix: Add ForceHttps middleware to enforce HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->prepend(\App\Http\Middleware\TrustProxies::class);
+        $middleware->prepend([
+            \App\Http\Middleware\ForceHttps::class,
+            \App\Http\Middleware\TrustProxies::class,
+        ]);
         $middleware->alias([
             'check.api.access' => \App\Http\Middleware\CheckApiAccess::class,
         ]);
