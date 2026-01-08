e9bb423776deb26493726708d1c5cd8857de3416
Nicholas Andrian Surjadi <surjadin@Mac.dana.local>
2026-01-08 14:29:22 +0700

    fix: Configure trusted proxies to fix HTTPS issue with uploads

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(\App\Http\Middleware\TrustProxies::class);
         $middleware->alias([
             'check.api.access' => \App\Http\Middleware\CheckApiAccess::class,
         ]);
